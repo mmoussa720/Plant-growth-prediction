897dac086ee1d571ad9403543e3bd46e37e7df2a
mmoussa720 <mohamed.moussa.0720@gmail.com>
2026-01-22 12:10:02 +0100

    Integrate Firebase Firestore for plant growth predictions and update dependencies

--- api.py
+++ api.py
@@ -1,7 +1,20 @@
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
-
+import os
+from firebase_admin import credentials, initialize_app,firestore
+firebase_config = {
+    "type": "service_account",
+    "project_id": os.getenv("project_id"),
+    "private_key_id": os.getenv("private_key_id"),
+    "private_key": os.getenv("private_key").replace("\\n", "\n"),
+    "client_email": os.getenv("client_email"),
+    "auth_provider_x509_cert_url": os.getenv("auth_provider_x509_cert_url"),
+    "token_uri": "https://oauth2.googleapis.com/token"
+}
+cred=credentials.Certificate("serviceaccount.json")
+initialize_app(cred)
+db=firestore.client()
 with open('plant_growth_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
@@ -19,7 +32,11 @@ def predict():
         df[col] = label_encoders[col].transform(df[col])
 
     prediction = int(model.predict(df)[0])
+    db.collection('predictions').add({
+        'input_data': data,
+        'date': firestore.SERVER_TIMESTAMP,
+        'predicted_growth_milestone': prediction
+    })
     return jsonify({'Growth_Milestone': prediction})
-print(Flask.__version__)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
