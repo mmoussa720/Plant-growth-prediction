61e2c64b96a77883941298b98523d5008ba32794
mmoussa720 <mohamed.moussa.0720@gmail.com>
2026-01-22 13:09:07 +0100

    Refactor Firebase configuration to load private key from environment variables

--- api.py
+++ api.py
@@ -1,13 +1,19 @@
 from flask import Flask, request, jsonify
 import pickle
+from dotenv import load_dotenv
 import pandas as pd
 import os
 from firebase_admin import credentials, initialize_app,firestore
+load_dotenv()
+
+private_key = os.getenv("private_key")
+if private_key is None:
+    raise ValueError("private_key not found in environment variables!")
 firebase_config = {
     "type": "service_account",
     "project_id": os.getenv("project_id"),
     "private_key_id": os.getenv("private_key_id"),
-    "private_key": os.getenv("private_key"),
+    "private_key": private_key.replace("\\n", "\n"),
     "client_email": os.getenv("client_email"),
     "auth_provider_x509_cert_url": os.getenv("auth_provider_x509_cert_url"),
     "token_uri": "https://oauth2.googleapis.com/token"
